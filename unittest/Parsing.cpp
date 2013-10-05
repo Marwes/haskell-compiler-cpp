fe5e8c8ac1a566746ba374f94bde687e6de922a8
Markus <marwes91@gmail.com>
2013-10-05 11:53:01 +0200

    Added test for 3 + 2 + 4

--- unittest/Parsing.cpp
+++ unittest/Parsing.cpp
@@ -3,13 +3,91 @@
 
 using namespace MyVMNamespace;
 
-TEST_CASE("parser", "Test parsing")
+namespace MyVMNamespace {
+    
+bool operator==(const Name& lhs, const Name& rhs)
+{
+    return lhs.name == rhs.name;
+}
+
+bool operator==(const Number& lhs, const Number& rhs)
+{
+    return lhs.value == rhs.value;
+}
+
+bool operator==(const FunctionApplication& lhs, const FunctionApplication& rhs)
+{
+    return lhs.function == rhs.function && lhs.arguments == rhs.arguments;
+}
+
+
+
+bool operator==(const Expression& lhs, const Expression& rhs)
+{
+    {
+        auto r = dynamic_cast<const FunctionApplication*>(&rhs);
+        auto l = dynamic_cast<const FunctionApplication*>(&lhs);
+        if (r && l)
+            return *r == *l;
+    }
+    
+    {
+        auto r = dynamic_cast<const Name*>(&rhs);
+        auto l = dynamic_cast<const Name*>(&lhs);
+        if (r && l)
+            return *r == *l;
+    }
+    {
+        auto r = dynamic_cast<const Number*>(&rhs);
+        auto l = dynamic_cast<const Number*>(&lhs);
+        if (r && l)
+            return *r == *l;
+    }
+    return false;
+}
+
+
+bool operator!=(const Expression& lhs, const Expression& rhs)
+{
+    return !(lhs == rhs);
+}
+
+}
+
+TEST_CASE("parser/3+2", "3 + 2")
 {
     std::stringstream stream("3 + 2");
     Tokenizer tokenizer(stream);
     Parser parser(tokenizer);
 
-    auto maybeExpression = parser.run();
-    REQUIRE (maybeExpression);
-    REQUIRE (typeid(*maybeExpression) == typeid(FunctionApplication));
+    std::unique_ptr<Expression> maybeExpression = parser.run();
+    REQUIRE (maybeExpression.get() != NULL);
+
+    FunctionApplication* func = dynamic_cast<FunctionApplication*>(maybeExpression.get());
+    REQUIRE (func != NULL);
+    REQUIRE (func->arguments.size() == 2);
+    REQUIRE (*func->arguments[0] == Number(3));
+    REQUIRE (*func->arguments[1] == Number(2));
+    
 }
+
+TEST_CASE("parser/3", "3 + 2 + 4")
+{
+    std::stringstream stream("3 + 2 + 4");
+    Tokenizer tokenizer(stream);
+    Parser parser(tokenizer);
+
+    auto maybeExpression = parser.run();
+    REQUIRE (maybeExpression.get() != NULL);
+
+    FunctionApplication* func = dynamic_cast<FunctionApplication*>(maybeExpression.get());
+    REQUIRE (func != NULL);
+    REQUIRE (func->arguments.size() == 2);
+    REQUIRE (*func->arguments[0] == Number(3));
+
+    const FunctionApplication* second = dynamic_cast<const FunctionApplication*>(func->arguments[1].get());
+    REQUIRE (second != NULL);
+    REQUIRE (*second->arguments[0] == Number(2));
+    REQUIRE (*second->arguments[1] == Number(4));
+}
+
