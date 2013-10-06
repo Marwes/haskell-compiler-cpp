2ff6ec8cb592536f4469ac51dccb9fbd0b0afe9a
Markus <marwes91@gmail.com>
2013-10-06 18:03:38 +0200

    Cleaned up source directory
    Removed haskell files added missing unittest/Compiler.cpp

---
+++ unittest/Compiler.cpp
@@ -0,0 +1,47 @@
+#include "Catch/include/catch.hpp"
+#include <VM.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "Compiler.h"
+#include "Util.h"
+#include "Method.h"
+#include "Array.h"
+#include "TestUtility.h"
+
+using namespace MyVMNamespace;
+
+TEST_CASE("compiler/arithmetic", "Test compiling an arithmetic expression")
+{
+    std::stringstream input("3+2");
+    Compiler compiler(input);
+    Assembly assembly;
+    assembly.entrypoint = 0;
+    assembly.instructions = compiler.compile();
+
+    {
+        VM vm;
+        Method method = Method::main(assembly);
+        MethodEnvironment env(vm.newStackFrame(), &method);
+        vm.execute(env);
+        REQUIRE(vm.getValue(0).intValue == 5);
+    }
+}
+
+
+TEST_CASE("compiler/arithmetic2", "Test compiling an arithmetic expression")
+{
+    std::stringstream input("2 * 4 + 3");
+    Compiler compiler(input);
+    Assembly assembly;
+    assembly.entrypoint = 0;
+    assembly.instructions = compiler.compile();
+
+    {
+        VM vm;
+        Method method = Method::main(assembly);
+        MethodEnvironment env(vm.newStackFrame(), &method);
+        vm.execute(env);
+        REQUIRE(vm.getValue(0).intValue == 11);
+    }
+}
