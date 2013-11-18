47121f1b2b85c626eeb433495701d82dbc05501f
Markus Westerlind <marwes91@gmail.com>
2013-11-18 13:38:00 +0100

    Added wrapper class around boost::variant for easier debugging
    It assigns a pointer to the current type to allow inspection in the debugger

--- include/Types.h
+++ include/Types.h
@@ -35,7 +35,56 @@ private:
 inline bool operator==(const TypeVariable& l, const TypeVariable& r) { return l.id == r.id; }
 inline bool operator!=(const TypeVariable& l, const TypeVariable& r) { return !(l == r); }
 
-typedef boost::variant<TypeVariable, TypeOperator> Type;
+typedef boost::variant<TypeVariable, boost::recursive_wrapper<TypeOperator>> TypeVariant;
+class Type : public TypeVariant
+{
+	//Type wrapper which gives pointers to the values in the variant for easier debugging
+public:
+	Type()
+	{
+		setDebugType();
+	}
+	Type(const TypeVariable& op)
+		: TypeVariant(op)
+	{
+		setDebugType();
+	}
+	Type(const TypeOperator& op)
+		: TypeVariant(op)
+	{
+		setDebugType();
+	}
+
+	Type& operator=(const Type& t)
+	{
+		TypeVariant::operator=(static_cast<const TypeVariant&>(t));
+		setDebugType();
+		return *this;
+	}
+	Type& operator=(Type&& t)
+	{
+		TypeVariant::operator=(std::move(static_cast<TypeVariant&>(t)));
+		setDebugType();
+		return *this;
+	}
+
+	void setDebugType()
+	{
+		if (which() == 0)
+		{
+			var = boost::get<TypeVariable>(this);
+			op = nullptr;
+		}
+		else
+		{
+			op = boost::get<TypeOperator>(this);
+			var = nullptr;
+		}
+	}
+private:
+	TypeVariable* var;
+	TypeOperator* op;
+};
 
 std::ostream& operator<<(std::ostream& s, const TypeVariable& type);
 std::ostream& operator<<(std::ostream& str, const TypeOperator& type);
@@ -77,6 +126,7 @@ public:
 	std::string name;
 	std::vector<Type> types;
 };
+inline bool operator==(const Type& l, const Type& r) { return l.operator==(static_cast<const TypeVariant&>(r)); }
 inline bool operator==(const TypeOperator& l, const TypeOperator& r) { return l.name == r.name && l.types == r.types; }
 inline bool operator!=(const TypeOperator& l, const TypeOperator& r) { return !(l == r); }
 
