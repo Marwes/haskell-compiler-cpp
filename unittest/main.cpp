12be70925aed29cb3ed4f0b530a9bb1de765814d
Markus Westerlind <marwes91@gmail.com>
2013-11-20 09:11:11 +0100

    Added ParseError exception to unittest context

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -1,8 +1,13 @@
 #define CATCH_CONFIG_RUNNER
 #include <assert.h>
 #include <direct.h>
+#include "Parser.h"
 #include "Catch/include/catch.hpp"
 
+std::string parseErrorTranslator(MyVMNamespace::ParseError& exc)
+{
+	return exc.what();
+}
 int main(int argc, char* const argv [])
 {
     _chdir("unittest");
@@ -11,6 +16,7 @@ int main(int argc, char* const argv [])
 	Catch::ConfigData data;
 	data.shouldDebugBreak = true;
 	Catch::Config config(data);
+	Catch::ExceptionTranslatorRegistrar::ExceptionTranslatorRegistrar(parseErrorTranslator);
 	int ret = Catch::Main(argc, argv, config);
     if (ret != 0)//Wait for input if something has gone wrong
     {
